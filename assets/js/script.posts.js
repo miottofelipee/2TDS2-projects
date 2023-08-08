0b7a873298c4530e44754243130132d84ab0865f
miottofelipee <123502192+miottofelipee@users.noreply.github.com>
2023-08-08 16:40:09 -0300

    finalizing the code with help

--- assets/js/script.posts.js
+++ assets/js/script.posts.js
@@ -1,88 +1,81 @@
 const posts = [];
+let postIndex = -1;
 function savePost() {
-    const title = document.getElementById("title").value
-    const category = document.getElementById("category").value
-    const resume = document.getElementById("resume").value
-    const author = document.getElementById("author").value
-    const date = document.getElementById("date").value
-    console.log(title, category, resume, author, date)
+    const tittle = document.getElementById("tittle").value;
+    const category = document.getElementById("category").value;
+    const resume = document.getElementById("resume").value;
+    const author = document.getElementById("author").value;
+    const date = document.getElementById("date").value;
+    if (tittle && category && resume && author && date) {
+        if (postIndex == -1) {
+            storePost(tittle, category, resume, author, date)
+            cleanFields();
+            showPosts();
+        } else {
+            posts[postIndex] = {
+                tittle,
+                category,
+                resume,
+                author,
+                date,
+            };
+        }
+        cleanFields();
+        showPosts();
+        postIndex = -1;
+    } else {
+        alert("Preencha todos os campos!");
+    }
 }
-
 function cleanFields() {
-    document.getElementById("title").value = "";
+    document.getElementById("tittle").value = "";
     document.getElementById("category").value = "";
     document.getElementById("resume").value = "";
     document.getElementById("author").value = "";
     document.getElementById("date").value = "";
 }
-
-if (title && category && resume && author && date) {
-    storePost(title, category, resume, author, date);
-    cleanFields();
-    showPosts();
-} else {
-    alert("Preencha certo!")
-}
-
-function storePost(title, category, resume, author, date) {
+function storePost(tittle, category, resume, author, date) {
     const post = {
-        title,
+        tittle,
         category,
         resume,
         author,
-        date
+        date,
     };
-    posts.push(post)
-
+    posts.push(post);
     console.log(posts)
 }
-
-function showPosts {
-    document.getElementById("list").classList.remove("hidden");
+function showPosts() {
+    document.getElementById("list").classList.remove("hidden")
     let showContent = "";
     posts.forEach((post, index) => {
         showContent += `
-        <div class="itemPost">
-        <h2>$(post.title)</h2>
-        <p><strong>Categoria: </strong> ${post.category}</p>
-        <p><strong>Resumo: </strong> ${post.resume}</p>
-        <p><strong>Autor: </strong> ${post.author}</p>
-        <p><strong>Data da publicação: </strong> ${post.date}</p>
-        <button onclick= "editPost(${index})">Editar</button>
-        <button onclick= "deletePost(${index})">Excluir</button>
-        </div>
-        `
-    })
-
+ <div class="itemPost">
+<h2>${post.tittle}</h2>
+<p><strong>Categoria:</strong>${post.category}</p>
+ <p><strong>Resumo:</strong>${post.resume}</p>
+ <p><strong>Author:</strong>${post.author}</p>
+ <p><strong>Data de publicação:</strong>${post.date}</p>
+ <button onclick="editPost(${index})">Editar</button>
+ <button onclick="removePost(${index})">Excluir</button>
+ </div>
+ `;
+    });
     document.getElementById("list").innerHTML = showContent;
 }
-
 function editPost(index) {
     const post = posts[index];
-    document.getElementById("title").value = post.title;
+    document.getElementById("tittle").value = post.tittle;
     document.getElementById("category").value = post.category;
     document.getElementById("resume").value = post.resume;
     document.getElementById("author").value = post.author;
     document.getElementById("date").value = post.date;
+    postIndex = index;
 }
-
-if (tittle && category && resume && author && date) {
-    if (postIndex == -1) {
-        storePost(tittle, category, resume, author, date)
-        cleanFields();
-        showPosts();
-    } else {
-        posts[postIndex] = {
-            tittle,
-            category,
-            resume,
-            author,
-            date,
-        }
-    }
-}
-
-function removePost{
+function removePost(index) {
     posts.splice(index, 1);
     showPosts();
+    if (posts.length == 0) {
+        document.getElementById("list").classList.add("hidden")
+    }
 }
