1710380e7b36fb0f4abfa9c68c5017e31ca79756
miottofelipee <123502192+miottofelipee@users.noreply.github.com>
2023-08-17 10:16:38 -0300

    add class product

--- assets/js/script.cetegory-produt.js
+++ assets/js/script.cetegory-produt.js
@@ -5,3 +5,12 @@ class Category{
         this.products = [];
     }
 }
+
+class Products{
+    constructor(id, name, price, category){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
